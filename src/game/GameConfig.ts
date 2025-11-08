7cf8871d3dfff2fe353544a99847378e7afc2ea9
Arjan Scherpenisse <arjan@miraclethings.nl>
2025-11-08 13:39:53 +0100

    feat: scale game 2x and create much longer level (100 tiles wide)

--- src/game/GameConfig.ts
+++ src/game/GameConfig.ts
@@ -11,8 +11,8 @@ export const GameConfig: Phaser.Types.Core.GameConfig = {
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: 960,
-    height: 540,
+    width: 480,  // Halved from 960 to make everything appear 2x bigger
+    height: 270, // Halved from 540 to make everything appear 2x bigger
     parent: 'game',
   },
   physics: {
